43d14d0da016acc56d561fb5692c19d6f3e377aa
WangXiang <xiang@debian-owncloud-opt>
2018-03-23 21:20:03 +0800

    fix bug

--- gamedemo/run.sh
+++ gamedemo/run.sh
@@ -2,12 +2,14 @@
 
 service mongodb start
 cd /game/server/etc
-python gen_conf.py 4000 $GAME_HOST_IP
+python gen_conf.py 4000 $GAME_HOST_IP $GAME_HOST_PORT docker
 cd ../bin
 bash run_server.sh
 cd ../script/rpyc_server
 bash run.sh
 tailon -b 0.0.0.0:8080 -f /game/server/log/*.log &
+
+echo $GAME_HOST_IP $GAME_HOST_PORT
 
 set -e
 /set_root_pw.sh
